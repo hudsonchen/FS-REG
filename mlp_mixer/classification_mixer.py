2e4411fd721bfdb95ed1351efdf5e491840eff2a
Hudson Chen <czh17@mails.tsinghua.edu.cn>
2021-09-21 21:47:36 +0800

    minor

--- mlp_mixer/classification_mixer.py
+++ mlp_mixer/classification_mixer.py
@@ -95,7 +95,7 @@ state = init_state
 
 # Optimizer Initialization
 def schedule_fn(learning_rate, n_batches):
-    epoch_points = [int(args.epochs * 0.3), int(args.epochs * 0.5), int(args.epochs * 0.7)]
+    epoch_points = [int(args.epochs * 0.3), int(args.epochs * 0.4), int(args.epochs * 0.6)]
     epoch_points = (jnp.array(epoch_points) * n_batches).tolist()
     return utils.piecewise_constant_schedule(learning_rate, epoch_points, args.lr_decay)
 
